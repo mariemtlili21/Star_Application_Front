dbcc3ae5ce20e5dc82a252d58d1b034689cf99a0
yassinemarzouki <92584248+yassinemarzouki@users.noreply.github.com>
2023-09-21 16:42:32 +0100

    retouche final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: '/home',
-  //   pathMatch: 'full'
-  // },
+   {
+     path: '',
+    redirectTo: '/home',
+     pathMatch: 'full'
+   },
  
   {path:'registration',component:RegisterComponent},
   {path:'login',component:LoginComponent},
+  {path:'sidebar',component:SidebarComponent},
+  {path:'footer',component:FooterComponent},
+  {path:'home',component:HomeComponent},
   
 
 ];
